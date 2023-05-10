41d672c551112c3d62b938588c72b8e86a731238
joshuaPhong <klx2044@yahoo.com>
2023-05-10 20:55:17 +1200

    w3 schools python notes

---
+++ Booleans.py
@@ -0,0 +1,23 @@
+# booleans return true or false
+#
+# Almost any value is evaluated to True if it has some sort of content.
+#
+# Any string is True, except empty strings.
+#
+# Any number is True, except 0.
+#
+# Any list, tuple, set, and dictionary are True, except empty ones.
+# the following will return false
+# bool(False)
+# bool(None)
+# bool(0)
+# bool("")
+# bool(())
+# bool([])
+# bool({})
+#
+# You can create functions that returns a Boolean Value:
+# def myFunction() :
+#   return True
+#
+# print(myFunction())
